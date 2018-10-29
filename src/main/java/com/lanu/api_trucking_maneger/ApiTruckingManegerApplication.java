41279ac46b63b72d3703197ced2693a44c3b78e8
Serhiy K <mrlanu@gmail.com>
2018-10-28 21:37:25 -0500

    init commit

---
+++ src/main/java/com/lanu/api_trucking_maneger/ApiTruckingManegerApplication.java
@@ -0,0 +1,12 @@
+package com.lanu.api_trucking_maneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTruckingManegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTruckingManegerApplication.class, args);
+    }
+}
